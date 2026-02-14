7fbfd4e636aafd534356262bc358376e569dcc8f
Lucas Axberg <lucas.axberg@gmail.com>
2026-02-14 18:06:12 +0100

    added read and skip for Reader, where index ends up as 'last_index + 1' when read or skipped to out of bounds + tests for functions

--- src/ID3.rs
+++ src/ID3.rs
@@ -26,6 +26,23 @@ impl Reader {
         self.bytes.append(&mut bytes.to_vec());
         self
     }
+
+    fn skip_n_bytes(&mut self, n: usize) -> (){
+        self.index = (self.index + n).min(self.bytes.len())
+    }
+
+    fn read_n_bytes(&mut self, n: usize) -> Vec<u8> {
+        let mut read_bytes = Vec::new();
+        for i in 0..n {
+            let Some(byte) = self.bytes.get(self.index) else {
+                break;
+            };
+            read_bytes.push(*byte);
+            self.index += 1;
+        }
+        read_bytes
+    }
+
 }
 
 #[cfg(test)]
@@ -39,5 +56,38 @@ mod tests {
     #[test]
     fn invalid_header() {
         assert!(!header_exists(&[0x49, 0x44, 0x33, 0x03, 0x00, 0x01, 0x00, 0x08, 0x2e, 0x37]))
+    }
+
+    #[test]
+    fn read_bytes_in_bounds() {
+        let mut reader = Reader::default().load(&[1, 2, 3, 4, 5]);
+        assert_eq!(reader.read_n_bytes(3), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn read_bytes_out_of_bounds() {
+        let mut reader = Reader::default().load(&[1, 2, 3, 4, 5]);
+        assert_eq!(reader.read_n_bytes(6), vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn skip_bytes_in_bounds() {
+        let mut reader = Reader::default().load(&[1, 2, 3, 4, 5]);
+        reader.skip_n_bytes(3);
+        assert_eq!(reader.index, 3);
+    }
+
+    #[test]
+    fn skip_bytes_out_of_bounds() {
+        let mut reader = Reader::default().load(&[1, 2, 3, 4, 5]);
+        reader.skip_n_bytes(7);
+        assert_eq!(reader.index, 5);
+    }
+
+    #[test]
+    fn read_bytes_out_of_bounds_index() {
+        let mut reader = Reader::default().load(&[1, 2, 3, 4, 5]);
+        let _ = reader.read_n_bytes(7);
+        assert_eq!(reader.index, 5);
     }
 } 
