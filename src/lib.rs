0ab503a9eeac3502f370f55b32fd831b8673c94f
Lucas Axberg <lucas.axberg@gmail.com>
2026-02-25 21:38:16 +0100

    implement failing test for 'tryfrom vec' on syncsafe

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,14 @@ impl From<[u8; 4]> for SyncSafe {
     }
 }
 
+impl TryFrom<Vec<u8>> for SyncSafe {
+    type Error = std::io::Error;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,5 +33,21 @@ mod tests {
     #[test]
     fn parse_sync_safe_from_invalid_bytes() {
         assert_eq!(SyncSafe::from([0b_11101110, 0b_11101110, 0b_11101110, 0b_11101110]).0, 0b_00001101_11011011_10110111_01101110);
+    }
+
+    #[test]
+    fn parse_sync_safe_from_valid_vec_of_valid_bytes() {
+        assert_eq!(SyncSafe::try_from(vec![0b_01101110, 0b_01101110, 0b_01101110, 0b_01101110]).unwrap().0, 0b_00001101_11011011_10110111_01101110)
+    }
+
+    #[test]
+    fn parse_sync_safe_from_valid_vec_of_invalid_bytes() {
+        assert_eq!(SyncSafe::try_from(vec![0b_11101110, 0b_11101110, 0b_11101110, 0b_11101110]).unwrap().0, 0b_00001101_11011011_10110111_01101110)
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_sync_safe_panics_from_invalid_vec() {
+        SyncSafe::try_from(vec![0b_11101110, 0b_11101110, 0b_11101110]).unwrap();
     }
 }
